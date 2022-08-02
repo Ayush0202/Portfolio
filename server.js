d20f9110a47bc0eb072d6ad9a4704c66d010a4cf
Ayush0202 <ayush.skg@gmail.com>
2022-08-02 23:42:19 +0530

    server added

---
+++ server.js
@@ -0,0 +1,24 @@
+/*jshint esversion: 6 */
+
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000')
+})
